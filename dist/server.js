023b6390e99ae9e393e458e71a6d49e0c6a57937
SamirBatista <samir_viana1@hotmail.com>
2023-03-24 21:31:10 -0300

    editServer

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const pg_helper_1 = require("./database/pg-helper");
-const port = process.env.PORT || 8080;
-const app = (0, express_1.default)();
-app.use(express_1.default.json(), (0, cors_1.default)()); //routes);
-pg_helper_1.pgHelper
-    .connect()
-    .then(() => app.listen(port, () => console.log(`Server is running in port ${port}`)))
-    .catch((err) => console.log(err));
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import {pgHelper} from "./database/pg-helper";
+// const port = process.env.PORT || 8080;
+// const app = express();
+// app.use(express.json(), cors()); //routes);
+// pgHelper
+//   .connect()
+//   .then(() =>
+//     app.listen(port, () => console.log(`Server is running in port ${port}`))
+//   )
+//   .catch((err) => console.log(err));
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import {pgHelper} from "./database/pg-helper";
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import {pgHelper} from "./database/pg-helper";
 
-const port = process.env.PORT || 8080;
-const app = express();
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-app.use(express.json(), cors()); //routes);
+// app.use(express.json(), cors()); //routes);
 
-pgHelper
-  .connect()
-  .then(() =>
-    app.listen(port, () => console.log(`Server is running in port ${port}`))
-  )
-  .catch((err) => console.log(err));
+// pgHelper
+//   .connect()
+//   .then(() =>
+//     app.listen(port, () => console.log(`Server is running in port ${port}`))
+//   )
+//   .catch((err) => console.log(err));
